4143100b9befaec677b26872fdaf6cdf846094d4
Bohdan <bshmalko97@gmail.com>
2020-10-21 20:36:16 +0300

    add first task

--- implementation.go
+++ implementation.go
@@ -1,9 +1,42 @@
 package lab2
 
-import "fmt"
+import (
+  "strings" //strings functions
+  "strconv" //string to number
+  "math" //for power
+  "errors" //for error
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func CalculatePostfix(input string) (float64, error) {
+
+  if input == "" { return -1, errors.New("!!! Enter something !!!") }
+
+  operators := map[string]func(float64, float64)(float64){
+    "+" : func (x, y float64) float64{return x + y},
+    "-" : func (x, y float64) float64{return x - y},
+    "*" : func (x, y float64) float64{return x * y},
+    "/" : func (x, y float64) float64{return x / y},
+    "^" : func (x, y float64) float64{return math.Pow(x, y)},
+  }
+
+  var stack []float64
+
+  strArr := strings.Fields(input)
+
+  for _, val := range strArr {
+    if action , found := operators[val]; found {
+      y := stack[len(stack) - 1]
+      stack = stack[:len(stack)-1]
+      x := stack[len(stack) - 1]
+      stack = stack[:len(stack)-1]
+      stack = append(stack, action(x,y))
+    } else {
+      number, err := strconv.ParseFloat(val, 64)
+      if err != nil {  return -1, errors.New("!!! You entered incorrect data !!!") }
+      stack = append(stack, number)
+    }
+  }
+
+  if len(stack) != 1 { return -1, errors.New("!!! You entered incorrect data !!!") }
+	return stack[len(stack) - 1], nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,44 @@
 package lab2
 
 import (
-	"fmt"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+    "testing"
+    gocheck "gopkg.in/check.v1"
+		"fmt"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type MySuite struct{}
+var _ = gocheck.Suite(&MySuite{})
+
+func (s *MySuite) TestCalculatePostfix(c *gocheck.C) {
+
+var testMap = map[string]float64 {
+	"2 2 8 - * 31 +" : 19, // 2 * (2 - 8) + 31
+	"146 275 + 438 + 200 -" : 659, //146 + 275 + 438 - 200
+	"435 5 / 411 3 * + 13 -" : 1307 , //435 / 5 + 411 * 3 - 13
+	"6 3 ^ 348 2 / + 610 3 * - 210 3 * + 3 6 ^ + 12 -" : -93, //6 ^ 3 + 348 / 2 - 610 * 3 + 210 * 3 + 3 ^ 6 - 12
+	"614 22300 2 / + 4 6 ^ + 25 5 ^ 12 / + 93 +" : 829755.0833333334, //614 + 22300 / 2 + 4 ^ 6 + 25 ^ 5 / 12 + 93
+	"33448 2212 16 / 12 2 ^ * + 16 4 / 28 * + 483 -" : 52985, //33448 + 2212 / 16 * 12 ^ 2 + 16 / 4 * 28 - 483
+	"ksksk sllsl " : -1,
+  "" : -1,
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+var res float64
+var err error
+for key , val := range testMap {
+	res, err = CalculatePostfix(key)
+	if err != nil {
+			fmt.Println("ERROR : ", err)
+		} else {
+			c.Assert(res, gocheck.Equals, val)
+		}
+  }
+}
+
+func ExampleCalculatePostfix() {
+	res, _ := CalculatePostfix("2 2 +")
 	fmt.Println(res)
 
-	// Output:
-	// 2 2 +
+	// Output: 4
 }
